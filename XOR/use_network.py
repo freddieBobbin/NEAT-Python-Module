58fe91b0f8ef91e0b8443da15682840989cf0317
freddieBobbin <73397769+freddieBobbin@users.noreply.github.com>
2022-04-16 18:35:51 +0100

    Update use_network.py

--- XOR/use_network.py
+++ XOR/use_network.py
@@ -2,7 +2,7 @@ from NEAT import NetLoader
 
 xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
 
-net = NetLoader.load_net("corys_net")
+net = NetLoader.load_net("filename")       # filename to load
 
 for inputs in xor_inputs:
     print(inputs, net.evaluate(inputs))
